8b25780427345b4390fb4ce5653c3eb73446d8af
GyeongHun <riverpower6@naver.com>
2026-01-17 13:40:57 +0900

    [SETTING] Spring Lock 실습 프로젝트 세팅

---
+++ src/main/java/io/river/spring_lock/SpringLockApplication.java
@@ -0,0 +1,13 @@
+package io.river.spring_lock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/river/spring_lock/SpringLockApplicationTests.java
@@ -0,0 +1,13 @@
+package io.river.spring_lock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
